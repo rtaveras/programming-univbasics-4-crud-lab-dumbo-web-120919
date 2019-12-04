e2046bd5e73dae468551a2edaebe6b6cbf50fb50
Riaan Taveras <riaantaveras@gmail.com>
2019-12-04 00:27:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  check = array.length-1
+  array[check]
 end
 
 def update_element_from_index(array, index_number, element)
